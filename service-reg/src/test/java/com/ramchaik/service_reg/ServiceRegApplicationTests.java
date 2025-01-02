7afeb090497955d923036185cbe9df40fff44af0
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2025-01-02 12:23:20 -0400

    feat: add eureka service registry service

---
+++ service-reg/src/test/java/com/ramchaik/service_reg/ServiceRegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramchaik.service_reg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
